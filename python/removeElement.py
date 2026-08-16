88599f2c12f861ce2a58a0ca57513594d6b52ff6
Yuvateja5I5 <yuvatejadara@gmail.com>
2026-08-16 22:46:37 +0530

    Add files via upload

---
+++ python/removeElement.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeElement(self, nums, val):
+        i=0
+        for num in nums:
+            if num!=val:
+                nums[i]=num
+                i+=1
+        return i
+s1=Solution()
+nums=[3,2,2,3]
+val=2
+k=s1.removeElement(nums,val)
+print(k)
+print(nums[:k])
+
+
+        
+    
+                
+    
+        
